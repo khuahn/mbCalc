cd24ffbaa60a6a6f9ec94fdb66cfb1d5f177bb6b
Khuahn <khuahn@gmail.com>
2025-08-31 14:11:15 -0500

    Added columns

--- index.php
+++ index.php
@@ -6,14 +6,35 @@ include 'header.php';
 
 <div class="container">
   <h1>MedBillCalc</h1>
-  <form id="calcForm">
-    <label for="charge">Charge Amount:</label>
-    <input type="number" id="charge" name="charge" required>
+  <table id="calcTable">
+    <thead>
+      <tr>
+        <th>Charges</th>
+        <th>Payments</th>
+        <th>Adjustment</th>
+        <th>Balance</th>
+      </tr>
+    </thead>
+    <tbody id="calcBody">
+      <tr>
+        <td><input type="number" name="charge[]"></td>
+        <td class="payment">0.00</td>
+        <td class="adjustment">0.00</td>
+        <td class="balance">0.00</td>
+      </tr>
+    </tbody>
+  </table>
 
-    <label for="allowed">Allowed Amount:</label>
-    <input type="number" id="allowed" name="allowed" required>
+  <div class="buttons">
+    <button id="addRow">Add Row</button>
+    <button id="clear">Clear</button>
+    <button id="print">Print</button>
+    <button id="darkMode">Dark Mode</button>
+  </div>
 
-    <label for="copay">Copay:</label>
-    <input type="number" id="copay" name="copay" required>
-
-    <label for="deductible">Deductible:</label>
+  <div id="totals">
+    <p>Total Payments: <span id="totalPayments">0.00</span></p>
+    <p>Total Adjustments: <span id="totalAdjustments">0.00</span></p>
+    <p>Total Balance: <span id="totalBalance">0.00</span></p>
+  </div>
+</div>
